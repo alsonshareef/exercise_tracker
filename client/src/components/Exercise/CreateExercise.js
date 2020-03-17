38fb7f116b25b52a04c980ef841323d5f66e385f
Alson Shareef <alson.shareef@gmail.com>
2020-03-17 18:20:59 +0800

    Form for adding exercises, onChange state handlers for form fields and submit handler of the form with currently logs created exercise.

---
+++ client/src/components/Exercise/CreateExercise.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
+    };
+  }
+
+  /* Life-cycle Hooks */
+  componentDidMount() {
+    this.setState({
+      users: ['test user'],
+      username: 'test user'
+    });
+  }
+
+  /* On Change Handlers */
+  onUsernameChange = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
+
+  onDescriptionChange = event => {
+    this.setState({
+      description: event.target.value
+    });
+  };
+
+  onDurationChange = event => {
+    this.setState({
+      duration: event.target.value
+    });
+  };
+
+  onDateChange = date => {
+    this.setState({
+      date: date
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    };
+
+    console.log(exercise);
+
+    window.location = '/';
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Create New Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onUsernameChange}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onDescriptionChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onDurationChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onDateChange}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Exercise Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
